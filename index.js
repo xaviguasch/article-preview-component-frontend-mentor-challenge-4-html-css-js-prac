a5f37ea4df1e28def228a71ae9e286de78dc42dc
xaviguasch <xaviguasch@gmail.com>
2021-03-24 12:08:11 +0100

    cleaned up js file

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const shareBtns = document.querySelectorAll('.share-btn')
-const authorArea = document.querySelector('.author-area')
 const shareArea = document.querySelector('.share-area')
 
 shareBtns.forEach((shareBtn) => {
   shareBtn.addEventListener('click', () => {
-    // authorArea.classList.toggle('hidden')
     shareArea.classList.toggle('visible')
   })
 })
-
-console.log(window.innerWidth)
